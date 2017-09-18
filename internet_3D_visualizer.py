87037ad1d92b631f12479f1ff52136286635d80f
minto <minto@hotmail.fr>
2017-09-18 09:20:47 +0200

    update

--- internet_3D_visualizer.py
+++ internet_3D_visualizer.py
@@ -1,21 +1,15 @@
 import os
-import warnings
+import os.path as op
 from inspect import stack
-try:
-    import simplekml
-except ImportError:
-    warnings.warn('simplekml not installed: export to google earth disabled')
-try:
-    import networkx as nx
-except ImportError:
-    warnings.warn('networkx missing: "pip install networkx"')
-        
-path_app = os.path.dirname(os.path.abspath(stack()[0][1]))
-path_graphs = os.path.join(path_app, 'dataset')
+import simplekml
+import networkx as nx
+
+path_app = op.dirname(op.abspath(stack()[0][1]))
+path_graphs = op.join(path_app, 'dataset')
     
 # style of a point in Google Earth
 point_style = simplekml.Style()
-point_style.labelstyle.color = simplekml.Color.blue
+point_style.labelstyle.color = simplekml.Color.white
 point_style.labelstyle.scale = 2
 point_style.iconstyle.icon.href = 'https://raw.githubusercontent.com/afourmy/pyNMS/master/Icons/default_router.gif'
 
@@ -28,9 +22,9 @@ node_coords = {}
 
 kml = simplekml.Kml()
 
-for file in os.listdir(os.path.join(path_app, 'dataset')):
+for file in os.listdir(op.join(path_app, 'dataset')):
     try:
-        graph = nx.read_gml(os.path.join(path_graphs, file))
+        graph = nx.read_gml(op.join(path_graphs, file))
     except nx.exception.NetworkXError:
         continue
     for name, properties in graph.node.items():
@@ -51,4 +45,4 @@ for file in os.listdir(os.path.join(path_app, 'dataset')):
         except KeyError:
             continue
         
-kml.save(os.path.join(path_app, 'google_earth_export.kml'))
+kml.save(op.join(path_app, 'google_earth_export.kml'))
